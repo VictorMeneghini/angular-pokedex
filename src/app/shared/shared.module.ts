80ba0eba6204af29c48c342309d67896ee0746af
Victor Meneghini <vhmeneghini@gmail.com>
2022-01-24 16:51:06 -0300

    feat: create scss about search component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,17 +4,21 @@ import { CommonModule } from '@angular/common';
 // Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    PokeHeaderComponent
+    PokeHeaderComponent,
+    PokeListComponent,
+    PokeSearchComponent
   ]
 })
 export class SharedModule { }
