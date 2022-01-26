8fa364866e1073e65f7959ee78c3f75e739716d0
Victor Meneghini <vhmeneghini@gmail.com>
2022-01-26 17:58:31 -0300

    refactor: adjustaments on audio-button

--- src/app/shared/audio-button/audio-button.component.ts
+++ src/app/shared/audio-button/audio-button.component.ts
@@ -8,21 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class AudioButtonComponent implements OnInit {
   public isPlaying:boolean = false;
   private audio:any = new Audio("../../../assets/sound/pokemon-theme.mp3");
+  public imageButtonPath:string = "../../../assets/icons/sound-play.svg"
 
   constructor() { }
 
   ngOnInit(): void {
+    this.audio.volume = 0.08
+    this.imageButtonPath = "./../../assets/icons/sound-play.svg"
   }
 
-  public playSound() {
-    this.audio.load();
+  public handleButtonClick() {
+    if(this.isPlaying) {
+      this.pauseSound()
+    } else {
+      this.playSound()
+    }
+  }
+
+  private playSound() {
     this.audio.play();
     this.isPlaying = true
+    this.imageButtonPath = "./../../assets/icons/sound-pause.svg"
   }
 
-  public pauseSound() {
+  private pauseSound() {
+    console.log(this.audio.duration)
     this.audio.pause()
+    this.imageButtonPath = "./../../assets/icons/sound-play.svg"
     this.isPlaying = false
   }
+
 
 }
