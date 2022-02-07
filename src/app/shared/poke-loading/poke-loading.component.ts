0abbdfa979be62bb14a45a42115b2aef99e8e4eb
Victor Meneghini <vhmeneghini@gmail.com>
2022-02-07 19:50:40 -0300

    feat: add guard
    * add login guard
    * add redirect links
    * improve login form

--- src/app/shared/poke-loading/poke-loading.component.ts
+++ src/app/shared/poke-loading/poke-loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-poke-loading',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-loading.component.scss']
 })
 export class PokeLoadingComponent implements OnInit {
+  @Input() size:string = ''
 
   constructor() { }
 
